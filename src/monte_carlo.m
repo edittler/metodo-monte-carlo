5b65e0b6b98444d6a39c410439ae3ea19d07c59c
Ezequiel Pérez Dittler <ezeperez26@users.noreply.github.com>
2016-04-19 20:49:14 -0300

    Corrijo el algoritmo y completo el informe

--- src/monte_carlo.m
+++ src/monte_carlo.m
@@ -1,6 +1,18 @@
-function r = monte_carlo(probabilidad, a, b, M, cant_simulaciones)
-  U = rand(1, cant_simulaciones);
-  Y = (U <= probabilidad);
-  nueva_probabilidad = sum(Y) / cant_simulaciones;
-  r = nueva_probabilidad * (b - a) * M;
-endfunction
+% Entrada
+a = 0;
+b = 2;
+M = 1;
+cant_simulaciones = 10000;
+
+% Genero los valores de X y de Y
+longitud_uniforme_x = b - a;
+X = rand(1, cant_simulaciones) * longitud_uniforme_x;
+Y = rand(1, cant_simulaciones) * M;
+
+% Aplico la funcion e ^ (- x ^2) y comparo con Y
+f_X = exp(-(X .^ 2));
+A = Y < f_X;
+
+% Calculo la probabilidad y finalmente el valor de la integral
+probabilidad = sum(A) / cant_simulaciones;
+integral = probabilidad * longitud_uniforme_x * M
